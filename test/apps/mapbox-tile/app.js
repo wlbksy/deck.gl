8feca5642a41ae4d95199b6803520b6b0656f891
Felix Palmer <felixpalmer@gmail.com>
2021-09-30 10:01:22 +0200

    Attempt to display lines

--- test/apps/mapbox-tile/app.js
+++ test/apps/mapbox-tile/app.js
@@ -2,12 +2,12 @@
 /* eslint-disable no-console */
 import React, {PureComponent} from 'react';
 import {render} from 'react-dom';
-import Tile from './Schema';
+import Tile from './Lines';
 import Protobuf from 'pbf';
 import DeckGL from '@deck.gl/react';
 import {MVTLayer, TileLayer} from '@deck.gl/geo-layers';
 import {GeoJsonLayer, PathLayer, PointCloudLayer, ScatterplotLayer} from '@deck.gl/layers';
-import {geojsonToBinary} from '@loaders.gl/gis';
+import {binaryToGeojson, geojsonToBinary} from '@loaders.gl/gis';
 
 // Set your mapbox token here
 const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
@@ -15,9 +15,9 @@ const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
 const INITIAL_VIEW_STATE = {
   bearing: 0,
   pitch: 0,
-  longitude: -73.986022,
-  latitude: 40.730743,
-  zoom: 10
+  longitude: -73.95643,
+  latitude: 40.8039,
+  zoom: 12
 };
 
 const COUNTRIES =
@@ -40,6 +40,8 @@ const USE_BINARY = true;
 const ALBERTO = true;
 const ALBERTO_URL =
   'http://10.0.32.237:8002/v3/maps/bq-bi-engine/table/{z}/{x}/{y}?mapId=cartobq._d296517907c39746c3a5652253a82ad3ee035be5.anon3c8918185b69854ef19bcfcd5afc498070e2dfbc&format=geojson&access_token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlJYVnRIYUdzaTUxMFZZYml1YjA5ZCJ9.eyJodHRwOi8vYXBwLmNhcnRvLmNvbS9lbWFpbCI6ImFsYmVydG9AY2FydG9kYi5jb20iLCJodHRwOi8vYXBwLmNhcnRvLmNvbS9hY2NvdW50X2lkIjoiYWNfbWs3bXV6Z3UiLCJpc3MiOiJodHRwczovL2F1dGgubG9jYWwuY2FydG8uY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTA4NDA5NTYzMzQxMzU5MDQxNjg0IiwiYXVkIjoiY2FydG8tY2xvdWQtbmF0aXZlLWFwaSIsImlhdCI6MTYzMjkxMjIzNCwiZXhwIjoxNjMyOTk4NjM0LCJhenAiOiJGM2tKOVJoUWhFTUFodDFRQllkQUluckRRTXJKVlI4dSIsInNjb3BlIjoicmVhZDpjdXJyZW50X3VzZXIiLCJwZXJtaXNzaW9ucyI6WyJhZG1pbjphY2NvdW50IiwicmVhZDphY2NvdW50IiwicmVhZDphcHBzIiwicmVhZDpjb25uZWN0aW9ucyIsInJlYWQ6Y3VycmVudF91c2VyIiwicmVhZDppbXBvcnRzIiwicmVhZDpsaXN0ZWRfYXBwcyIsInJlYWQ6bWFwcyIsInJlYWQ6dGlsZXNldHMiLCJyZWFkOnRva2VucyIsInVwZGF0ZTpjdXJyZW50X3VzZXIiLCJ3cml0ZTphcHBzIiwid3JpdGU6Y29ubmVjdGlvbnMiLCJ3cml0ZTppbXBvcnRzIiwid3JpdGU6bGlzdGVkX2FwcHMiLCJ3cml0ZTptYXBzIiwid3JpdGU6dG9rZW5zIl19.SPs4WJHjwa8X8Nz8-4noZU2xQmZ8N52XZh3Gmea18-aCBQBUh9BML8WcpBYDD_LU9a02V2uG8Xp4otnkz-C1gA7idMmynthQAYSeRQWslImbjR5BwYW7l6XMTJ3fF2a2MRC6gQCtgfN45OYagvzNNBcQEn6Fffcs79BUkQsdhRctFp5AN1SU7ixevly24_BJM56vX0ihCstFhaoQiDQCX7R7MHNLFIk1RXb2xDC-3inhUzw94wetPHcQNBr5MiLQfNmJYVq_oemU7bVGsT2iIvZIghhypBy__eA_z0uwtiEAekC_01JQ-9v1_TjewmLs30qzyfNonKX32nVcjKiJuw';
+const ALBERTO_URL2 =
+  'http://10.0.32.237:8002/v3/maps/bq-bi-engine/table/{z}/{x}/{y}?format=geojson&geomType=lines&name=cartobq.testtables.lines&access_token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlJYVnRIYUdzaTUxMFZZYml1YjA5ZCJ9.eyJodHRwOi8vYXBwLmNhcnRvLmNvbS9lbWFpbCI6ImFsYmVydG9AY2FydG9kYi5jb20iLCJodHRwOi8vYXBwLmNhcnRvLmNvbS9hY2NvdW50X2lkIjoiYWNfbWs3bXV6Z3UiLCJpc3MiOiJodHRwczovL2F1dGgubG9jYWwuY2FydG8uY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTA4NDA5NTYzMzQxMzU5MDQxNjg0IiwiYXVkIjoiY2FydG8tY2xvdWQtbmF0aXZlLWFwaSIsImlhdCI6MTYzMjkxMjIzNCwiZXhwIjoxNjMyOTk4NjM0LCJhenAiOiJGM2tKOVJoUWhFTUFodDFRQllkQUluckRRTXJKVlI4dSIsInNjb3BlIjoicmVhZDpjdXJyZW50X3VzZXIiLCJwZXJtaXNzaW9ucyI6WyJhZG1pbjphY2NvdW50IiwicmVhZDphY2NvdW50IiwicmVhZDphcHBzIiwicmVhZDpjb25uZWN0aW9ucyIsInJlYWQ6Y3VycmVudF91c2VyIiwicmVhZDppbXBvcnRzIiwicmVhZDpsaXN0ZWRfYXBwcyIsInJlYWQ6bWFwcyIsInJlYWQ6dGlsZXNldHMiLCJyZWFkOnRva2VucyIsInVwZGF0ZTpjdXJyZW50X3VzZXIiLCJ3cml0ZTphcHBzIiwid3JpdGU6Y29ubmVjdGlvbnMiLCJ3cml0ZTppbXBvcnRzIiwid3JpdGU6bGlzdGVkX2FwcHMiLCJ3cml0ZTptYXBzIiwid3JpdGU6dG9rZW5zIl19.SPs4WJHjwa8X8Nz8-4noZU2xQmZ8N52XZh3Gmea18-aCBQBUh9BML8WcpBYDD_LU9a02V2uG8Xp4otnkz-C1gA7idMmynthQAYSeRQWslImbjR5BwYW7l6XMTJ3fF2a2MRC6gQCtgfN45OYagvzNNBcQEn6Fffcs79BUkQsdhRctFp5AN1SU7ixevly24_BJM56vX0ihCstFhaoQiDQCX7R7MHNLFIk1RXb2xDC-3inhUzw94wetPHcQNBr5MiLQfNmJYVq_oemU7bVGsT2iIvZIghhypBy__eA_z0uwtiEAekC_01JQ-9v1_TjewmLs30qzyfNonKX32nVcjKiJuw';
 const ANTONIO_URL =
   'http://10.0.32.226:8002/v3/maps/dev-bigquery/table/{z}/{x}/{y}?mapId=cartodb-gcp-backend-data-team._d296517907c39746c3a5652253a82ad3ee035be5.anon3c8918185b69854ef19bcfcd5afc498070e2dfbc&format=geojson&access_token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlJYVnRIYUdzaTUxMFZZYml1YjA5ZCJ9.eyJodHRwOi8vYXBwLmNhcnRvLmNvbS9lbWFpbCI6ImFjb3J0ZXNAY2FydG9kYi5jb20iLCJodHRwOi8vYXBwLmNhcnRvLmNvbS9hY2NvdW50X2lkIjoiYWNfZGEwdmpmZyIsImlzcyI6Imh0dHBzOi8vYXV0aC5sb2NhbC5jYXJ0by5jb20vIiwic3ViIjoiZ29vZ2xlLW9hdXRoMnwxMDMyMzk1MzA1MDY5OTQ5NTM3NzUiLCJhdWQiOiJjYXJ0by1jbG91ZC1uYXRpdmUtYXBpIiwiaWF0IjoxNjMyOTI0OTM4LCJleHAiOjE2MzMwMTEzMzgsImF6cCI6IkYza0o5UmhRaEVNQWh0MVFCWWRBSW5yRFFNckpWUjh1Iiwic2NvcGUiOiJyZWFkOmN1cnJlbnRfdXNlciIsInBlcm1pc3Npb25zIjpbImFkbWluOmFjY291bnQiLCJyZWFkOmFjY291bnQiLCJyZWFkOmFwcHMiLCJyZWFkOmNvbm5lY3Rpb25zIiwicmVhZDpjdXJyZW50X3VzZXIiLCJyZWFkOmltcG9ydHMiLCJyZWFkOmxpc3RlZF9hcHBzIiwicmVhZDptYXBzIiwicmVhZDp0aWxlc2V0cyIsInJlYWQ6dG9rZW5zIiwidXBkYXRlOmN1cnJlbnRfdXNlciIsIndyaXRlOmFwcHMiLCJ3cml0ZTpjb25uZWN0aW9ucyIsIndyaXRlOmltcG9ydHMiLCJ3cml0ZTpsaXN0ZWRfYXBwcyIsIndyaXRlOm1hcHMiLCJ3cml0ZTp0b2tlbnMiXX0.RhMht6LutV9NjEvgfyVvLMN-avUvJYowLyozMSGbjyZe59JRzd51Fv5U3W45qHIXQhMv8wj86sTDX6zsH0uMy3NOg6UsTClqwKIRNutkmQqTkNA2gJ1Qk6Dkc_Yj08RsW6jTyQhBZVTw5T6wZutmm3LcLlLHb9dMX0TrfRwblD3f-YHMxF8UVL-O8WUV9VI3rE5cwmYeP1qbGNrVkxjG3pquHXzTo_b8CrDVGbbGlVyh9M30YAYjC0rHGaPWaCqEtu6eRJPvClHqbD5qQQinCEMzS4PDh5_pyMtC1XX3gclV1mb4rlnWE7YWoywyEp4szy1__fz8tRXQp0K32VS6Ng';
 
@@ -115,22 +117,29 @@ function generatePointIndices(coords) {
 }
 
 function tileToBinary(tile) {
-  const ids = generatePointIndices(tile.coords);
+  const pointPositions = (tile && tile.coords) || [];
+  const ids = generatePointIndices(pointPositions);
+  const linesPositions = (tile && tile.positions) || [];
+  let linesPathIndices = (tile && tile.pathIndices) || [];
+
+  const linesIds = [...linesPathIndices];
   const value = {
     points: {
-      positions: {value: new Float32Array(tile.coords), size: coordLength},
+      positions: {value: new Float32Array(pointPositions), size: coordLength},
       globalFeatureIds: {value: ids, size: 1},
       featureIds: {value: ids, size: 1},
       numericProps: {},
-      properties: []
+      properties: [],
+      type: 'Point'
     },
     lines: {
-      positions: {value: new Float32Array(), size: 2},
-      pathIndices: {value: new Uint16Array(), size: 1},
-      globalFeatureIds: {value: new Uint16Array(), size: 1},
-      featureIds: {value: new Uint16Array(), size: 1},
+      positions: {value: new Float32Array(linesPositions), size: 2},
+      pathIndices: {value: new Uint16Array(linesPathIndices), size: 1},
+      globalFeatureIds: {value: new Uint16Array(linesIds), size: 1},
+      featureIds: {value: new Uint16Array(linesIds), size: 1},
       numericProps: {},
-      properties: []
+      properties: [],
+      type: 'LineString'
     },
     polygons: {
       positions: {value: new Float32Array(), size: 2},
@@ -139,7 +148,8 @@ function tileToBinary(tile) {
       globalFeatureIds: {value: new Uint16Array(), size: 1},
       featureIds: {value: new Uint16Array(), size: 1},
       numericProps: {},
-      properties: []
+      properties: [],
+      type: 'Polygon'
     }
   };
 
@@ -148,7 +158,7 @@ function tileToBinary(tile) {
 
 function createTile() {
   return new TileLayer({
-    data: ALBERTO ? ALBERTO_URL : ANTONIO_URL,
+    data: ALBERTO ? ALBERTO_URL2 : ANTONIO_URL,
     autoHighlight: true,
     highlightColor: [60, 60, 60, 40],
     minZoom: 0,
@@ -168,6 +178,8 @@ function createTile() {
       const binaryData = USE_BINARY
         ? tileToBinary(props.data)
         : geojsonToBinary(props.data.features);
+      const geojson = binaryToGeojson(binaryData);
+      //const binary2 =
       return [
         new GeoJsonLayer({
           id: `${props.id}-geojson`,
@@ -176,6 +188,12 @@ function createTile() {
           stroked: true,
           filled: true,
           pointType: 'circle',
+          // iconAtlas: './spider-apple.png',
+          // iconMapping: {
+          //   marker: {x: 0, y: 0, width: 160, height: 160, mask: true}
+          // },
+          // getIcon: d => 'marker',
+          // getIconSize: 8,
           pointRadiusUnits: 'pixels',
           lineWidthMinPixels: 0.5,
           getPointRadius: 1.5,
