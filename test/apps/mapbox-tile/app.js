34fdb9476799a43de7951f278b702e103626ff0d
Felix Palmer <felixpalmer@gmail.com>
2021-12-02 13:02:10 +0100

    Support geojson in CBT

--- test/apps/mapbox-tile/app.js
+++ test/apps/mapbox-tile/app.js
@@ -14,7 +14,7 @@ import {getPolygonSignedArea} from '@math.gl/polygon';
 import {MVTLoader} from '@loaders.gl/mvt';
 import {binaryToGeojson, geojsonToBinary, TEST_EXPORTS} from '@loaders.gl/gis';
 import {classifyRings, featuresToBinary} from '@loaders.gl/mvt';
-import {geojson} from './testdata.js';
+import {testdata} from './testdata.js';
 
 const INITIAL_VIEW_STATE = {longitude: -73.95643, latitude: 40.8039, zoom: 9};
 const COUNTRIES =
@@ -32,18 +32,19 @@ const geomType = params.get('geomType') || 'polygons'; // points | lines | polyg
 //const token =
 //  'eyJhbGciOiJIUzI1NiJ9.eyJhIjoiYWNfZmt0MXdsbCIsImp0aSI6IjNmM2NlMjA3In0.zzfm2xZSAjcTlLxaPQHDy8uVJbGtEC5gItOg8U_gfP4';
 const token =
-  'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InlscXg0SVg3ek1oaUR1OFplSUlFSyJ9.eyJodHRwOi8vYXBwLmNhcnRvLmNvbS9lbWFpbCI6ImZwYWxtZXIrY2hyb21lQGNhcnRvZGIuY29tIiwiaHR0cDovL2FwcC5jYXJ0by5jb20vYWNjb3VudF9pZCI6ImFjX2ZrdDF3bGwiLCJpc3MiOiJodHRwczovL2F1dGguZGV2LmNhcnRvLmNvbS8iLCJzdWIiOiJhdXRoMHw2MWEwZDgyMGJkMDA3OTAwNzExNDViYTciLCJhdWQiOlsiY2FydG8tY2xvdWQtbmF0aXZlLWFwaSIsImh0dHBzOi8vY2FydG8tZGVkaWNhdGVkLWVudi51cy5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNjM4MzUxNTQ1LCJleHAiOjE2Mzg0Mzc5NDUsImF6cCI6IkczcTdsMlVvTXpSWDhvc2htQXVzZWQwcGdRVldySkdQIiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCByZWFkOmN1cnJlbnRfdXNlciB1cGRhdGU6Y3VycmVudF91c2VyIHJlYWQ6Y29ubmVjdGlvbnMgd3JpdGU6Y29ubmVjdGlvbnMgcmVhZDptYXBzIHdyaXRlOm1hcHMgcmVhZDphY2NvdW50IiwicGVybWlzc2lvbnMiOlsicmVhZDphY2NvdW50IiwicmVhZDphcHBzIiwicmVhZDpjb25uZWN0aW9ucyIsInJlYWQ6Y3VycmVudF91c2VyIiwicmVhZDppbXBvcnRzIiwicmVhZDpsaXN0ZWRfYXBwcyIsInJlYWQ6bWFwcyIsInJlYWQ6dGlsZXNldHMiLCJyZWFkOnRva2VucyIsInVwZGF0ZTpjdXJyZW50X3VzZXIiLCJ3cml0ZTphcHBzIiwid3JpdGU6Y29ubmVjdGlvbnMiLCJ3cml0ZTppbXBvcnRzIiwid3JpdGU6bWFwcyIsIndyaXRlOnRva2VucyJdfQ.dURvqXvIWthB6E1e_mxCs1i2K9nv_3BRveOlpRHhcoDkMOo0XNaw6lM6317vwHVRHXedYgLniZB64r-3QIBk9S3f-sJIajTcD57tsntIPvU3RnrTBvglD75XWMBevnIH1KswPQYSedpu6BKe5YW1Ypq1Oev2u1Cnr8bEmMDNezll0CbthYZ7YZxbf9gy4I4rHvBNVVY5W9RtUsuc7lABr8T30kij7IOHtaJPqwRo8f6OUnrfVL99CdCk51iS6ZPRa_NAAF_QBM8gHcs_KEf26ezRPeQRCrCuElqQ9qdK1P2-PNFN52nKSCCBBH07-g1OULQGbXDd8VDsfvl0Vv6Qrg';
+  'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InlscXg0SVg3ek1oaUR1OFplSUlFSyJ9.eyJodHRwOi8vYXBwLmNhcnRvLmNvbS9lbWFpbCI6ImZwYWxtZXIrY2hyb21lQGNhcnRvZGIuY29tIiwiaHR0cDovL2FwcC5jYXJ0by5jb20vYWNjb3VudF9pZCI6ImFjX2ZrdDF3bGwiLCJpc3MiOiJodHRwczovL2F1dGguZGV2LmNhcnRvLmNvbS8iLCJzdWIiOiJhdXRoMHw2MWEwZDgyMGJkMDA3OTAwNzExNDViYTciLCJhdWQiOlsiY2FydG8tY2xvdWQtbmF0aXZlLWFwaSIsImh0dHBzOi8vY2FydG8tZGVkaWNhdGVkLWVudi51cy5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNjM4NDQxMDY0LCJleHAiOjE2Mzg1Mjc0NjQsImF6cCI6IkczcTdsMlVvTXpSWDhvc2htQXVzZWQwcGdRVldySkdQIiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCByZWFkOmN1cnJlbnRfdXNlciB1cGRhdGU6Y3VycmVudF91c2VyIHJlYWQ6Y29ubmVjdGlvbnMgd3JpdGU6Y29ubmVjdGlvbnMgcmVhZDptYXBzIHdyaXRlOm1hcHMgcmVhZDphY2NvdW50IiwicGVybWlzc2lvbnMiOlsicmVhZDphY2NvdW50IiwicmVhZDphcHBzIiwicmVhZDpjb25uZWN0aW9ucyIsInJlYWQ6Y3VycmVudF91c2VyIiwicmVhZDppbXBvcnRzIiwicmVhZDpsaXN0ZWRfYXBwcyIsInJlYWQ6bWFwcyIsInJlYWQ6dGlsZXNldHMiLCJyZWFkOnRva2VucyIsInVwZGF0ZTpjdXJyZW50X3VzZXIiLCJ3cml0ZTphcHBzIiwid3JpdGU6Y29ubmVjdGlvbnMiLCJ3cml0ZTppbXBvcnRzIiwid3JpdGU6bWFwcyIsIndyaXRlOnRva2VucyJdfQ.GORlsUyCVJggGjJcq-56B_Va_lpD85HEgjFlRn-CSFdS_8oS1_znONGRGWfjheps4RvQKW6f7hZii07ZQOeTHgwbr9WyrFqubBXPN1uKBQIg2_uKEVV8ITaOgt5NJtDvSPWyehoO1Fh6k4HpLtseIhNRkwpcPLWG0zpORdr9e9vd3XpRTzbXwmEuuFLUH2T0RpR9ex6A3H62aV1HeVsEwcwnMr_Vy3gI6S73oCa_i2G4R5X1fYFesGfAf6-94Zm6ogUfuE423usp5ehakDDhsKm6KrbmgamBG3cX7ajlGwH2T_wwlRc30L7zd9mzbTJJGzFKsoNDfbT04c14GNFvQA';
 
 function buildUrl({formatTiles}) {
   return `${apiBaseUrl}/v3/maps/${connection}/table/{z}/{x}/{y}?name=${table}&cache=&access_token=${token}&formatTiles=${formatTiles}&geomType=${geomType}`;
 }
 
+const geojson = true;
 const wip = true;
 const showBasemap = true;
 const showTile = false;
-const showCBT = false;
+const showCBT = true;
 const showMVT = false;
-const showGeojson = true;
+const showGeojson = false;
 
 function Root() {
   const [binary, setBinary] = useState(false);
@@ -240,6 +241,7 @@ const parseJSON = arrayBuffer => {
 
 const parseCBT = (arrayBuffer, options) => {
   if (!arrayBuffer) return null;
+  if (geojson) return parseJSON(arrayBuffer);
   const tile = wip ? parseJSON(arrayBuffer) : parsePbf(arrayBuffer);
   const binary = tileToBinary(tile);
   return binary;
@@ -285,9 +287,11 @@ CBTLayer.layerName = 'CBTLayer';
 CBTLayer.defaultProps = {...MVTLayer.defaultProps, loaders: [CBTLoader]};
 
 function createCBT({clip, skipOdd}) {
+  const formatTiles = geojson ? 'geojson' : wip ? 'wip' : 'binary';
+
   return new CBTLayer({
     id: 'cbt',
-    data: buildUrl({formatTiles: wip ? 'wip' : 'binary'}),
+    data: buildUrl({formatTiles}),
 
     // Styling (same props as MVTLayer)
     getFillColor: [33, 171, 251],
@@ -419,7 +423,7 @@ function geojsonToBinary2(features) {
 function createGeojson({binary}) {
   return new GeoJsonLayer({
     id: 'geojson',
-    data: binary ? geojsonToBinary2(geojson.features) : geojson,
+    data: binary ? geojsonToBinary2(testdata.features) : testdata,
     stroked: true,
     lineWidthMinPixels: 0.5,
     getFillColor: [0, 171, 255],
